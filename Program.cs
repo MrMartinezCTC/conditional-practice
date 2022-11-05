fc82399b96fe64a5035ec72a76ef9dbea2d99dca
MrMartinezCTC <106337782+MrMartinezCTC@users.noreply.github.com>
2022-11-05 18:44:14 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,71 @@
-﻿using System;
+using System;
 
 class Program
 {
     static void Main(string[] args)
     {
+        CheckForPositiveNegativeZero();
+        Console.WriteLine("----------------------------------------------");
+
+        // Uncomment the two lines below to test your "FindMinimum" code
+        // FindMininum();
+        // Console.WriteLine("----------------------------------------------");
+        
+        // Uncomment the two lines below to test your "FindMaximum" code
+        // FindMaximum();
+        // Console.WriteLine("----------------------------------------------");
+        
+        // Uncomment the two lines below to test your "IsDivisibleBy5" code
+        // IsDivisibleBy5();
+        // Console.WriteLine("----------------------------------------------");
+        
+        // Uncomment the two lines below to test your "IsEven" code
+        // IsEven();
+        // Console.WriteLine("----------------------------------------------");
+        
+        // Uncomment the line below to test your "CharacterIsAVowel" code
+        // CharacterIsAVowel();
+    }
+
+    public static void CheckForPositiveNegativeZero()
+    {
+        // Write the code for your "CheckForPositiveNegativeZero" assignment below
+
+    }
+
+    public static void FindMininum()
+    {
+        // Write the code for your "FindMininum" assignment below
+
+    }
+
+    public static void FindMaximum()
+    {
+        // Write the code for your "FindMaximum" assignment below
+
+    }
+
+    public static void IsDivisibleBy5()
+    {
+        // Write the code for your "IsDivisibleBy5" assignment below
+
+    }
+
+    public static void IsEven()
+    {
+        // Write the code for your "IsEven" assignment below
+
+    }
+
+    public static void CharacterIsAVowel()
+    {
+        // Write the code for your "CharacterIsAVowel" assignment below
+
+    }
+
+    public static void PrintDayOfTheWeek()
+    {
+        // Write the code for your "PrintDayOfTheWeek" assignment below
         
     }
 }
